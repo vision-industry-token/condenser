fbedbaac0ceb759743c02322ed9854c63bb09474
Ali Monhi <monhi@protonmail.com>
2019-07-26 10:34:41 -0400

    Added share images. Updated site desc, welcome intro, and sidebar.

--- src/app/components/elements/SidebarNewUsers.jsx
+++ src/app/components/elements/SidebarNewUsers.jsx
@@ -4,7 +4,7 @@ import { SIGNUP_URL } from 'shared/constants';
 const SidebarNewUsers = () => (
     <div className="c-sidebar__module">
         <div className="c-sidebar__header">
-            <h3 className="c-sidebar__h3">New to vit.tube?</h3>
+            <h3 className="c-sidebar__h3">New to Touchit.Social?</h3>
         </div>
         <div className="c-sidebar__content">
             <ul className="c-sidebar__list">
@@ -18,9 +18,9 @@ const SidebarNewUsers = () => (
                 <li className="c-sidebar__list-item">
                     <a
                         className="c-sidebar__link"
-                        href="https://vicetoken.com/"
+                        href="https://visionindustry.com/"
                     >
-                        The blockchain
+                        Learn more...
                     </a>
                 </li>
                 {/*
