c8a69edeaf119e83b16229bdf25c09ce83de5250
Landen Danyluk <landendanyluk@landens-mbp.lan>
2019-11-12 23:49:05 -0600

    Improve Advertisement logic

--- src/app/components/elements/Advertisement.jsx
+++ src/app/components/elements/Advertisement.jsx
@@ -7,42 +7,90 @@ export default class Advertisement extends React.Component {
         style: PropTypes.object,
         desktop: PropTypes.array,
         mobile: PropTypes.array,
+        allSizes: PropTypes.array,
         alt: PropTypes.string,
     };
 
+    hideOnError(env) {
+        env.target.style = 'display:none';
+    }
+
     render() {
-        const { unit, style, desktop, mobile, alt } = this.props;
+        const { unit, style, desktop, mobile, allSizes, alt } = this.props;
+        let linkAllSizes,
+            srcAllSizes,
+            linkDesktop,
+            linkMobile,
+            srcDesktop,
+            srcMobile;
+        let ads = [];
 
-        const randDesktop =
-            desktop.length === 1
-                ? 0
-                : Math.floor(Math.random() * desktop.length);
-        const randMobile =
-            mobile.length === 1 ? 0 : Math.floor(Math.random() * mobile.length);
+        if (allSizes) {
+            const rand =
+                allSizes.length === 1
+                    ? 0
+                    : Math.floor(Math.random() * allSizes.length);
+            linkAllSizes = allSizes[rand].href || '#';
+            srcAllSizes = allSizes[rand].src || '#';
 
-        const linkDesktop = desktop[randDesktop].href || '#';
-        const srcDesktop = desktop[randDesktop].src || '#';
-        const linkMobile = mobile[randMobile].href || '#';
-        const srcMobile = mobile[randMobile].src || '#';
+            const adAllSizes = (
+                <div>
+                    <a href={linkAllSizes}>
+                        <img
+                            src={srcAllSizes}
+                            alt={alt || 'Advertisement'}
+                            onError={this.hideOnError}
+                        />
+                    </a>
+                </div>
+            );
+            ads.push(adAllSizes);
+        } else {
+            if (desktop) {
+                const rand =
+                    desktop.length === 1
+                        ? 0
+                        : Math.floor(Math.random() * desktop.length);
+                linkDesktop = desktop[rand].href || '#';
+                srcDesktop = desktop[rand].src || '#';
 
-        const adDesktop = (
-            <div className="desktop">
-                <a href={linkDesktop}>
-                    <img src={srcDesktop} alt={alt || 'Advertisement'} />
-                </a>
-            </div>
-        );
-        const adMobile = (
-            <div className="mobile">
-                <a href={linkMobile}>
-                    <img src={srcMobile} alt={alt || 'Advertisement'} />
-                </a>
-            </div>
-        );
+                const adDesktop = (
+                    <div className="desktop">
+                        <a href={linkDesktop}>
+                            <img
+                                src={srcDesktop}
+                                alt={alt || 'Advertisement'}
+                                onError={this.hideOnError}
+                            />
+                        </a>
+                    </div>
+                );
+                ads.push(adDesktop);
+            }
+
+            if (mobile) {
+                const rand =
+                    mobile.length === 1
+                        ? 0
+                        : Math.floor(Math.random() * mobile.length);
+                linkMobile = mobile[rand].href || '#';
+                srcMobile = mobile[rand].src || '#';
+                const adMobile = (
+                    <div className="mobile">
+                        <a href={linkMobile}>
+                            <img
+                                src={srcMobile}
+                                alt={alt || 'Advertisement'}
+                                onError={this.hideOnError}
+                            />
+                        </a>
+                    </div>
+                );
+                ads.push(adMobile);
+            }
+        }
+
         const cls = `ad ${unit}`;
-        return React.createElement('div', { className: cls, style }, [
-            adDesktop,
-            adMobile,
-        ]);
+        return React.createElement('div', { className: cls, style }, ads);
     }
 }
